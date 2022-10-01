69ea6e5de13c7e402c8411ab2b2262b3ab9dd60a
roy656 <v0o0605@gmail.com>
2022-10-02 00:22:48 +0900

    JobExecutionContext 와 StepExecutionContext 의 사용 범위에 관한 테스트용 클래스 생성

--- src/main/java/practice/springbatch/sprignbatch/SprignBatchApplication.java
+++ src/main/java/practice/springbatch/sprignbatch/SprignBatchApplication.java
@@ -1,8 +1,10 @@
 package practice.springbatch.sprignbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class SprignBatchApplication {
 
